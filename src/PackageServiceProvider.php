65d211238fcc703ca0c080b2106de33afcba4669
Mikhaylov.V <mihailov.v@fhclub.ru>
2020-10-20 10:35:05 +0300

    rename package

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Vladmeh\Package;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
